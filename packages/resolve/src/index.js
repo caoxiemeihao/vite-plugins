b2599804786dd45f09cc4488e20f651652cd4702
草鞋没号 <308487730@qq.com>
2022-01-12 12:51:38 +0800

    refactor: use alias instead load-hook

--- packages/resolve/src/index.js
+++ packages/resolve/src/index.js
@@ -1,34 +1,123 @@
+const fs = require('fs');
+const path = require('path');
 
 /**
- * Custo  m resolve code for vite
- * @type {(external: Record<string, string | () => string>) => import('vite').Plugin}
+ * @typedef {{(dict: Record<string, string | () => string>) => import('vite').Plugin}} ViteResolve 
+ */
+
+/**
+ * Custom resolve code for vite
+ * @type {ViteResolve}
  * @example
  * export default defineConfig({
  *   plugins: [
  *     viteResolve({
- *       // use string
+ *       // use code string
  *       vue: `const vue = window.Vue; export default vue;`,
- *       // use function to return string
- *       vuex: () => `const vuex = window.Vuex; export default Vuex;`,
+ *       // use function to return code string
+ *       '@scope/name': () => `const Lib = window.LibraryName; export default Lib;`,
  *     })
  *   ]
  * })
  */
-function viteResolve(external = {}) {
+module.exports = function (dict = {}) {
+  let root = process.cwd();
+  const resolvePluginDirectory = '.vite-plugin-resolve';
+
   return {
     name: 'vite-plugin-resolve',
-    enforce: 'pre',
-    resolveId(id) {
-      if (external[id]) {
-        return id
+    config(config) {
+      // init root path
+      if (config.root && path.isAbsolute(config.root)) {
+        // TODO: config.root is relative path
+        root = config.root;
       }
-    },
-    load(id) {
-      if (external[id]) {
-        return typeof external[id] === 'function' ? external[id]() : external[id]
-      }
+
+      // step-1
+      generateReolveFile(
+        root,
+        resolvePluginDirectory,
+        dict
+      );
+
+      // step-2
+      rewriteAlias(
+        config,
+        root,
+        resolvePluginDirectory,
+        dict
+      );
     },
   }
 }
 
-module.exports = viteResolve
+function generateReolveFile(
+  root,
+  directory,
+  resolveDict
+) {
+  const dir = path.join(node_modules(root), directory);
+
+  // ensure .vite-plugin-resolve directory existed
+  fs.existsSync(dir) || fs.mkdirSync(dir);
+
+  // generate external module file
+  for (const [module, strOrFn] of Object.entries(resolveDict)) {
+    const moduleId = path.join(dir, module + '.js');
+
+    if (!fs.existsSync(moduleId)) {
+      // for '@scope/name' module
+      ensureDir(path.parse(moduleId).dir);
+
+      const moduleContent = typeof strOrFn === 'function' ? strOrFn() : strOrFn;
+
+      fs.writeFileSync(moduleId, moduleContent);
+    }
+  }
+}
+
+function rewriteAlias(
+  config,
+  root,
+  directory,
+  resolveDict
+) {
+  if (!config.resolve) {
+    config.resolve = {};
+  }
+
+  let alias = config.resolve.alias || {};
+  if (!Array.isArray(alias)) {
+    alias = Object.entries(alias).map(([k, v]) => ({ find: k, replacement: v }));
+  }
+
+  Object.keys(resolveDict).forEach(k => {
+    // redirect resolve module to `node_modules/.vite-plugin-resolve` directory
+    alias.push({ find: k, replacement: path.join(node_modules(root), directory, k) });
+  });
+
+  config.resolve.alias = alias;
+}
+
+function node_modules(root, count = 0) {
+  if (node_modules.p) {
+    return node_modules.p;
+  }
+  const p = path.join(root, 'node_modules');
+  if (fs.existsSync(p)) {
+    return node_modules.p = p;
+  }
+  if (count >= 19) {
+    throw new Error('Can not found node_modules directory.');
+  }
+  return node_modules(path.join(root, '..'), count + 1);
+}
+
+function ensureDir(dir) {
+  try {
+    fs.statSync(dir).isDirectory();
+  } catch (error) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+  return dir;
+}
