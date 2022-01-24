e427c6f2a433e26e2c5a869107374c4bf68195dd
草鞋没号 <308487730@qq.com>
2022-01-24 09:37:43 +0800

    optimized code

--- packages/fast-external/index.js
+++ packages/fast-external/index.js
@@ -1,12 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function external(
   externals,
   options = {},
 ) {
-  let root = process.cwd()
-  const viteExternalId = '.vite-plugin-fast-external'
+  let root = process.cwd();
+  let externalDir = '.vite-plugin-fast-external';
 
   return {
     name: 'vite-plugin-fast-external',
@@ -14,84 +14,90 @@ module.exports = function external(
       // init root path
       if (config.root && path.isAbsolute(config.root)) {
         // TODO: config.root is relative path
-        root = config.root
+        root = config.root;
       }
 
-      rewriteAlias(root, config, externals, viteExternalId)
-      generateExternalFile(root, externals, viteExternalId, options?.format || 'esm')
+      // absolute path
+      externalDir = path.join(node_modules(root), externalDir);
+
+      rewriteAlias(
+        config,
+        externals,
+        externalDir,
+      );
+      generateExternalFile(
+        externals,
+        externalDir,
+        options?.format || 'esm',
+      );
     }
   }
 }
 
 function ensureDir(dir) {
   if (!(fs.existsSync(dir) && fs.statSync(dir).isDirectory())) {
-    fs.mkdirSync(dir, { recursive: true })
+    fs.mkdirSync(dir, { recursive: true });
   }
-  return dir
+  return dir;
 }
 
 function node_modules(root, count = 0) {
   if (node_modules.p) {
-    return node_modules.p
+    return node_modules.p;
   }
-  const p = path.join(root, 'node_modules')
+  const p = path.join(root, 'node_modules');
   if (fs.existsSync(p)) {
-    return node_modules.p = p
+    return node_modules.p = p;
   }
   if (count >= 19) {
-    throw new Error('Can not found node_modules directory.')
+    throw new Error('Can not found node_modules directory.');
   }
-  return node_modules(path.join(root, '..'), count + 1)
+  return node_modules(path.join(root, '..'), count + 1);
 }
 
 function generateExternalFile(
-  root,
   externals,
-  directory,
-  format
+  externalDir,
+  format,
 ) {
-  // ensure .vite-plugin-fast-external directory existed
-  const externalDir = path.join(node_modules(root), directory)
-  fs.existsSync(externalDir) || fs.mkdirSync(externalDir)
-
-  // generate external module file.
+  // generate external module file
   for (const [module, strOrFn] of Object.entries(externals)) {
-    const moduleId = path.join(node_modules(root), directory, `${module}.js`)
-    let moduleContent
+    const moduleId = path.join(externalDir, `${module}.js`);
+    let moduleContent;
     if (typeof strOrFn === 'string') {
-      const iifeName = strOrFn
+      const iifeName = strOrFn;
       moduleContent = format === 'cjs'
         ? `const ${iifeName} = window['${iifeName}']; module.exports = ${iifeName};`
-        : `const ${iifeName} = window['${iifeName}']; export { ${iifeName} as default }`
+        : `const ${iifeName} = window['${iifeName}']; export { ${iifeName} as default }`;
     } else {
-      moduleContent = strOrFn()
+      moduleContent = strOrFn();
     }
 
     // for '@scope/name' package
-    ensureDir(path.parse(moduleId).dir)
-    fs.writeFileSync(moduleId, moduleContent)
+    ensureDir(path.parse(moduleId).dir);
+    fs.writeFileSync(moduleId, moduleContent);
   }
 }
 
 function rewriteAlias(
-  root,
   config,
-  external,
-  directory
+  externals,
+  externalDir,
 ) {
   if (!config.resolve) {
-    config.resolve = {}
+    config.resolve = {};
   }
 
-  let alias = config.resolve.alias || {}
+  let alias = config.resolve.alias || {};
   if (!Array.isArray(alias)) {
-    alias = Object.entries(alias).map(([k, v]) => ({ find: k, replacement: v }))
+    // keep the the original alias
+    alias = Object.entries(alias).map(([k, v]) => ({ find: k, replacement: v }));
   }
 
-  Object.keys(external).forEach(k => {
-    // redirect external module to `node_modules/.vite-plugin-fast-external` directory
-    alias.push({ find: k, replacement: path.join(node_modules(root), directory, k) })
+  Object.keys(externals).forEach(k => {
+    // redirect external-module to `node_modules/.vite-plugin-fast-external`
+    alias.push({ find: k, replacement: path.join(externalDir, k) });
   })
 
-  config.resolve.alias = alias
+  config.resolve.alias = alias;
 }
