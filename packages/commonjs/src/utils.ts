6c4ea2d39672bf11fb10afa218f7c9be4852a193
草鞋没号 <308487730@qq.com>
2022-02-22 15:58:05 +0800

    add DEFAULT_EXTENSIONS, isCommonjs

--- packages/commonjs/src/utils.ts
+++ packages/commonjs/src/utils.ts
@@ -1,6 +1,11 @@
 
+export const DEFAULT_EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx', '.vue']
+
 export function cleanUrl(url: string) {
   return url.replace(/\?.*$/s, '').replace(/#.*$/s, '')
 }
 
-export const DEFAULT_EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx', '.vue']
+export function isCommonjs(code: string) {
+  // TODO: ignore comments
+  return /\b(?:require|module|exports)\b/.test(code)
+}
