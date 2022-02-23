8507749dbf4c3fbb0348e7b568ae3c799712d3ed
草鞋没号 <308487730@qq.com>
2022-02-23 10:47:24 +0800

    Move commonjs to after known plugins

--- packages/commonjs/src/index.ts
+++ packages/commonjs/src/index.ts
@@ -1,8 +1,9 @@
 import path from 'path'
 import { Plugin, ResolvedConfig } from 'vite'
 import {
-  cleanUrl,
   DEFAULT_EXTENSIONS,
+  KNOWN_PLUGINS,
+  cleanUrl,
   isCommonjs,
 } from './utils'
 import cjs2esm from './cjs-esm'
@@ -17,12 +18,33 @@ export interface CommonjsOptions {
   ignore?: (...args: Parameters<Plugin['transform']>) => boolean
 }
 
-export function commonjs(options: CommonjsOptions = {}): Plugin {
+export default function commonjs(options: CommonjsOptions = {}): Plugin {
+  const COMMONJS_PLUGIN_NAME = 'vite-plugin-commonjs'
   let config: ResolvedConfig
 
   return {
-    name: 'vite-plugin-commonjs',
+    name: COMMONJS_PLUGIN_NAME,
     apply: 'serve',
+    config(_config) {
+      /**
+       * 'vite-plugin-commonjs' can only transform JavaScript.
+       * So it should be put behind some known plugins.
+       */
+      const plugins = _config.plugins as Plugin[]
+      const knownNames = Object.values(KNOWN_PLUGINS).flat()
+      const pluginNames = plugins.map(plugin => plugin.name)
+      const orderIndex = pluginNames.reverse().findIndex(name => knownNames.includes(name))
+      if (orderIndex !== -1) {
+        const commonjsIndex = pluginNames.findIndex(name => name === COMMONJS_PLUGIN_NAME)
+        const commonjsPlugin = plugins.splice(commonjsIndex, 1)[0]
+        if (commonjsIndex < orderIndex) { // It is located before a known plugin
+          // Move it to after known plugins
+          plugins.splice(orderIndex + 1, 0, commonjsPlugin)
+        }
+        _config.plugins = plugins
+        return _config
+      }
+    },
     configResolved(_config) {
       config = _config
     },
