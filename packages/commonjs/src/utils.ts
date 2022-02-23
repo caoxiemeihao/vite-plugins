5bb311df1f3a21be2ca9e6265c13269277558ebc
草鞋没号 <308487730@qq.com>
2022-02-23 10:42:53 +0800

    add KNOWN_PLUGINS

--- packages/commonjs/src/utils.ts
+++ packages/commonjs/src/utils.ts
@@ -1,11 +1,30 @@
 
 export const DEFAULT_EXTENSIONS = ['.js', '.jsx', '.ts', '.tsx', '.vue']
+export const KNOWN_PLUGINS = {
+  '@vitejs/plugin-vue': [
+    'vite:vue',
+  ],
+  '@vitejs/plugin-vue-jsx': [
+    'vite:vue-jsx',
+  ],
+  '@vitejs/plugin-react': [
+    'vite:react-babel',
+    'vite:react-refresh',
+    'vite:react-jsx',
+  ],
+  'vite-plugin-vue2': [
+    'vite-plugin-vue2',
+  ],
+  '@sveltejs/vite-plugin-svelte': [
+    'vite-plugin-svelte',
+  ],
+}
 
 export function cleanUrl(url: string) {
   return url.replace(/\?.*$/s, '').replace(/#.*$/s, '')
 }
 
 export function isCommonjs(code: string) {
-  // TODO: ignore comments
+  // Avoid matching the content of the comment
   return /\b(?:require|module|exports)\b/.test(code)
 }
