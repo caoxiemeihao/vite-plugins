07179a9bc13b7add6f4f076a55a79360674bf7b5
草鞋没号 <308487730@qq.com>
2022-03-05 07:15:35 +0800

    fix: module name value error

--- packages/fast-external/index.js
+++ packages/fast-external/index.js
@@ -10,7 +10,8 @@ module.exports = function external(externals, options = {}) {
 
   for (const [moduleId, strOrFn] of Object.entries(externals)) {
     if (typeof strOrFn === 'string') {
-      externals[moduleId] = `const ${moduleId} = window['${/* iife name */strOrFn}']; export { ${moduleId} as default }`;
+      const iifeName = strOrFn;
+      externals[moduleId] = `const ${iifeName} = window['${iifeName}']; export { ${iifeName} as default }`;
     }
   }
 
