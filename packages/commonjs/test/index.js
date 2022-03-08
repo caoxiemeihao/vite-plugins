a9e53cce7a0243842d6c599dfcd93d9cbfa83816
草鞋没号 <308487730@qq.com>
2022-03-08 09:54:13 +0800

    cjs instead esm

--- packages/commonjs/test/index.js
+++ packages/commonjs/test/index.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import Module from '../cjs-esm/Module';
+const fs = require('fs');
+const path = require('path');
+const Module = require('../dist/cjs-esm/Module').default;
 
 const requireJs = path.join(__dirname, 'require.js');
 const code = fs.readFileSync(requireJs, 'utf8');
