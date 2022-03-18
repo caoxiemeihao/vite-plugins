c5b951e2202a3bcd490aa046be1f0d8cdd048003
草鞋没号 <308487730@qq.com>
2022-03-18 18:37:43 +0800

    create dynamic-import-helper.ts

---
+++ packages/dynamic-import/src/dynamic-import-helper.ts
@@ -0,0 +1,33 @@
+export interface DynamicImportRuntime {
+  name: string
+  body: string
+}
+
+export function generateDynamicImportRuntime(
+  entries: Record<string, string[]>,
+  dynamicImportIndex: number | string,
+): DynamicImportRuntime {
+  const _entries = Object.entries(entries)
+  const name = `__variableDynamicImportRuntime${dynamicImportIndex}__`
+  const cases = _entries.map(([localFile, importeeList]) => {
+    const c = importeeList.map(importee => `    case '${importee}':`)
+    return `${c.join('\n')}
+    return import('${localFile}');
+`
+  })
+  const body = `
+function ${name}(path) {
+  switch (path) {
+${cases.join('\n')}
+    default: return new Promise(function(resolve, reject) {
+      (typeof queueMicrotask === 'function' ? queueMicrotask : setTimeout)(
+        reject.bind(null, new Error("Unknown variable dynamic import: " + path))
+      );
+    })
+   }
+ }
+ 
+ `
+
+  return { name, body }
+}
