c9babd85b5ada4065220df5caf56430951b74392
草鞋没号 <308487730@qq.com>
2022-03-20 07:49:53 +0800

    refactor: v2.0.0

--- packages/resolve/index.d.ts
+++ packages/resolve/index.d.ts
@@ -1,47 +1,12 @@
-import { Plugin, UserConfig } from 'vite';
+import { Plugin } from 'vite';
 
-declare const resolve: VitePluginResolve;
 export default resolve;
-
-export interface ResolveArgs {
-  /** Generated file cache directory */
-  dir: string;
-}
-
-export interface Resolves {
-  [moduleId: string]:
-  | string
-  | ((args: ResolveArgs) =>
-    | string
-    | Promise<string | void>
-    | void)
-  | void;
-}
-
-export interface ResolveOptions {
-  /**
-   * Absolute path or relative path
-   * @default ".vite-plugin-resolve"
-   */
-  dir?: string;
-}
+declare const resolve: VitePluginResolve;
 
 export interface VitePluginResolve {
-  (resolves: Resolves, options?: ResolveOptions): Plugin;
-}
-
-// --------- utils ---------
-export interface GenerateESModule {
-  (dir: string, resolves: Resolves): Promise<void>;
-}
-
-export interface ModifyAlias {
-  (
-    config: UserConfig,
-    aliaList: { [moduleId: string]: string; }[],
-  ): void;
-}
-
-export interface ModifyOptimizeDepsExclude {
-  (config: UserConfig, exclude: string[]): void;
+  (entries: {
+    [moduleId: string]:
+      | ReturnType<Plugin['load']>
+      | ((...args: Parameters<Plugin['load']>) => ReturnType<Plugin['load']>)
+  }): Plugin[];
 }
--- packages/resolve/index.js
+++ packages/resolve/index.js
@@ -1,99 +1,44 @@
-const fs = require('fs');
-const path = require('path');
-
 /**
  * @type {import('.').VitePluginResolve}
  */
-module.exports = function resolve(resolves = {}, options = {}) {
-  let { dir = '.vite-plugin-resolve' } = options;
-  let root = process.cwd();
+module.exports = function resolve(resolves) {
+  const prefix = '\0';
+  const resolveKeys = Object.keys(resolves);
+  const resolveKeysWithPrefix = resolveKeys.map(key => prefix + key);
 
-  return {
-    name: 'vite-plugin-resolve',
-    async config(config) {
-      if (!path.isAbsolute(dir)) dir = path.join(node_modules(root), dir);
-      if (config.root) root = path.resolve(config.root);
-
-      modifyOptimizeDepsExclude(config, Object.keys(resolves));
-      modifyAlias(
-        config,
-        Object.keys(resolves).map(moduleId => ({ [moduleId]: path.join(dir, moduleId) })),
-      );
-
-      await generateESModule(dir, resolves);
+  return [
+    {
+      name: 'vite-plugin-resolve:resolveId',
+      // run before the builtin 'vite:resolve' of Vite
+      enforce: 'pre',
+      resolveId(source) {
+        if (resolveKeys.includes(source)) {
+          // https://vitejs.dev/guide/api-plugin.html#virtual-modules-convention
+          return prefix + source;
+        }
+      },
     },
-  }
-}
+    {
+      name: 'vite-plugin-resolve',
+      config(config) {
+        if (!config.optimizeDeps) config.optimizeDeps = {};
+        if (!config.optimizeDeps.exclude) config.optimizeDeps.exclude = [];
 
-/**
- * @type {import('.').GenerateESModule}
- */
-async function generateESModule(dir, resolves) {
-  // generate custom-resolve module file
-  for (const [module, strOrFn] of Object.entries(resolves)) {
-    const moduleId = path.join(dir, module + '.js');
-    const moduleContent = await (typeof strOrFn === 'function' ? strOrFn({ dir }) : strOrFn);
-    if (moduleContent == null) continue;
+        let keys = resolveKeys;
+        if (config.optimizeDeps.include) {
+          keys = resolveKeys.filter(key => !config.optimizeDeps.include.includes(key));
+        }
 
-    // supported nest moduleId ('@scope/name')
-    ensureDir(path.parse(moduleId).dir);
-    // write custom-resolve
-    fs.writeFileSync(moduleId, moduleContent);
-  }
-}
-
-/**
- * @type {import('.').ModifyAlias}
- */
-function modifyAlias(config, aliaList) {
-  if (!config.resolve) config.resolve = {};
-
-  let alias = config.resolve.alias || [];
-  if (!Array.isArray(alias)) {
-    // keep the the original alias
-    alias = Object.entries(alias).map(([k, v]) => ({ find: k, replacement: v }));
-  }
-  // redirect resolve-module to `node_modules/.vite-plugin-resolve`
-  alias.push(...aliaList.map(item => {
-    const [find, replacement] = Object.entries(item)[0];
-    return { find, replacement };
-  }));
-
-  config.resolve.alias = alias;
-}
-
-/**
- * @type {import('.').ModifyOptimizeDepsExclude}
- */
-function modifyOptimizeDepsExclude(config, exclude) {
-  if (!config.optimizeDeps) config.optimizeDeps = {};
-  if (!config.optimizeDeps.exclude) config.optimizeDeps.exclude = [];
-
-  if (config.optimizeDeps.include) {
-    exclude = exclude.filter(e => !config.optimizeDeps.include.includes(e));
-  }
-  config.optimizeDeps.exclude.push(...exclude);
-}
-
-// --------- utils ---------
-
-function ensureDir(dir) {
-  if (!(fs.existsSync(dir) && fs.statSync(dir).isDirectory())) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-  return dir;
-}
-
-function node_modules(root, count = 0) {
-  if (node_modules.p) {
-    return node_modules.p;
-  }
-  const p = path.join(root, 'node_modules');
-  if (fs.existsSync(p)) {
-    return node_modules.p = p;
-  }
-  if (count >= 19) {
-    throw new Error('Can not found node_modules directory.');
-  }
-  return node_modules(path.join(root, '..'), count + 1);
-}
+        config.optimizeDeps.exclude.push(...keys);
+      },
+      async load(id, opts) {
+        if (resolveKeysWithPrefix.includes(id)) {
+          const stringOrFunction = resolves[id.replace(prefix, '')];
+          return typeof stringOrFunction === 'function'
+            ? await stringOrFunction(id, opts)
+            : stringOrFunction;
+        }
+      },
+    },
+  ];
+};
