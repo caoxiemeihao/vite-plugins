a8401192e4972b087ee82143986be534df53c266
草鞋没号 <308487730@qq.com>
2022-03-20 08:59:57 +0800

    fix: sorter

--- packages/dynamic-import/src/sort-plugin.ts
+++ packages/dynamic-import/src/sort-plugin.ts
@@ -7,7 +7,7 @@ import type {
  * 'vite-plugin-dynamic-import' can only transform JavaScript.  
  * So it should be put behind some known plugins.
  */
- export function sortPlugin(pluginName: string, config: UserConfig): UserConfig {
+ export function sortPlugin(toSortPlugin: string, config: UserConfig): UserConfig {
   const KNOWN_PLUGINS = {
     '@vitejs/plugin-vue': 'vite:vue',
     'vite-plugin-vue2': 'vite-plugin-vue2',
@@ -20,17 +20,22 @@ import type {
     ],
   }
 
-  const plugins = config.plugins as Plugin[]
+  const plugins = [...config.plugins] as Plugin[]
   const knownNames = Object.values(KNOWN_PLUGINS).flat()
   const pluginNames = plugins.map(plugin => plugin.name)
-  const orderIndex = pluginNames.reverse().findIndex(name => knownNames.includes(name))
+
+  // Find the last known plugin
+  let orderIndex = [...pluginNames].reverse().findIndex(name => knownNames.includes(name))
+
   if (orderIndex !== -1) {
-    const pluginIndex = pluginNames.findIndex(name => name === pluginName)
-    const plugin = plugins.splice(pluginIndex, 1)[0]
+    // In the correct position in the array
+    orderIndex = pluginNames.length - 1 - orderIndex
+
+    const pluginIndex = pluginNames.findIndex(name => name === toSortPlugin)
     if (pluginIndex < orderIndex) {
       // It is located before a known plugin
       // Move it to after known plugins
-      plugins.splice(orderIndex, 0, plugin)
+      plugins.splice(orderIndex, 0, plugins.splice(pluginIndex, 1)[0])
     }
     config.plugins = plugins
     return config
