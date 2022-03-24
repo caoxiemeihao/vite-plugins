b197f56078f73a8d3a616f9fb9763dfb61328082
草鞋没号 <308487730@qq.com>
2022-03-24 09:34:08 +0800

    refactor: simpler code

--- packages/electron-renderer/index.d.ts
+++ packages/electron-renderer/index.d.ts
@@ -1,45 +1,8 @@
-import { Plugin, UserConfig } from 'vite';
+import { Plugin } from 'vite';
 
 declare const electronRenderer: VitePluginElectronRenderer;
 export default electronRenderer;
 
-export interface Resolve {
-  [moduleId: string]: string | (() => string | Promise<string>);
-}
-
 export interface VitePluginElectronRenderer {
-  (options?: {
-    /**
-     * @example
-     * {
-     *   resolve: {
-     *     'electron-store': 'const Store=require("electron-store"); export { Store as default }',
-     *   }
-     * }
-     */
-    resolve?: Resolve;
-  }): Plugin;
-}
-
-export interface GenerateESModule {
-  (cacheDir: string, resolve: Resolve): Promise<void>;
-}
-
-export interface ModifyAlias {
-  (
-    config: UserConfig,
-    aliaList: { [moduleId: string]: string; }[],
-  ): void;
-}
-
-export interface ModifyOptimizeDepsExclude {
-  (config: UserConfig, exclude: string[]): void;
-}
-
-export interface ModifyRollupExternal {
-  (config: UserConfig): void;
-}
-
-export interface ModifyOptionsForElectron {
-  (config: UserConfig): void;
+  (): Plugin[];
 }
--- packages/electron-renderer/index.js
+++ packages/electron-renderer/index.js
@@ -1,201 +1,78 @@
-const fs = require('fs');
 const path = require('path');
 const { builtinModules } = require('module');
+const optimizer = require('vite-plugin-optimizer');
 
 /**
  * @type {import('.').VitePluginElectronRenderer}
  */
-module.exports = function (options = {}) {
-  const { resolve } = options;
-  let root = process.cwd();
+module.exports = function () {
   const name = 'vite-plugin-electron-renderer';
-  const browserExternalId = '__vite-browser-external'
-  const ElectronRendererModule = path.join(__dirname, 'modules/electron-renderer.js');
+  const plugin = optimizer(builtinModulesExport(builtinModules), { dir: `.${name}` });
+  plugin.name = name;
 
-  return {
-    name,
-    async config(config, env) {
-      if (config.root && path.isAbsolute(config.root)) {
-        // TODO: config.root is relative path
-        root = config.root;
-      }
+  return [
+    {
+      name: `${name}:config`,
+      config(config) {
+        // make sure that Electron can be loaded into the local file using `loadFile` after packaging
+        if (!config.base) config.base = './';
 
-      modifyOptionsForElectron(config);
-      modifyOptimizeDepsExclude(config, ['electron', ...(resolve ? Object.keys(resolve) : [])]);
+        if (!config.build) config.build = {};
 
-      // in 'vite serve' phase ensure the 'electron' and NodeJs built-in modules are loaded correctly by `resolve.alias`
-      if (env.command === 'serve') {
-        modifyAlias(config, [{ electron: ElectronRendererModule }]);
-      }
-      // in 'vite build' phase insert 'electron' and NodeJs built-in modules into Rollup `output.external`
-      else if (env.command === 'build') {
-        modifyRollupExternal(config);
-      }
+        // ensure that static resources are loaded normally
+        if (!config.build.assetsDir) config.build.assetsDir = '';
 
-      // generate resolve-module file to `node_modules/.vite-plugin-electron-renderer`
-      // point resolve-module to this path by `resolve.alias`
-      if (resolve) {
-        const cacheDir = path.join(node_modules(root), `.${name}`);
-        modifyAlias(
-          config,
-          Object.keys(resolve).map(moduleId => ({ [moduleId]: path.join(cacheDir, moduleId) })),
-        );
-        await generateESModule(cacheDir, resolve);
-      }
-    },
-    configureServer(server) {
-      // intercept node built-in modules require, convert to ESModule respond
-      server.middlewares.use((req, res, next) => {
-        if ((req.url || '').includes(browserExternalId)) {
-          const builtinId = req.url.split(`${browserExternalId}:`, 2)[1];
-          if (builtinModules.includes(builtinId)) {
-            const builtinModule = require(builtinId);
-            const members = Object.keys(builtinModule);
-            const code = `
-const _M = require("${builtinId}");
-const {
-  ${members.join(',\n  ')}
-} = _M;
-
-export { _M as default, ${members.join(', ')} }
-`;
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Content-Type', 'application/javascript');
-            res.setHeader('Cache-Control', 'max-age=3600'); // an hour of cache
-            res.end(code);
-            return;
+        if (!config.build.rollupOptions) config.build.rollupOptions = {};
+        if (!config.build.rollupOptions.output) config.build.rollupOptions.output = {};
+        if (Array.isArray(config.build.rollupOptions.output)) {
+          config.build.rollupOptions.output.forEach(output => {
+            if (!output.format) {
+              // the packaged Electron app should use "cjs"
+              output.format = 'cjs';
+            }
+          });
+        } else {
+          if (!config.build.rollupOptions.output.format) {
+            // the packaged Electron app should use "cjs"
+            config.build.rollupOptions.output.format = 'cjs';
           }
         }
-        next();
-      });
+
+        // ----------------------------------------
+
+        if (!config.resolve) config.resolve = {};
+        if (!config.resolve.alias) config.resolve.alias = [];
+        const electronjs = path.join(__dirname, 'modules/electron-renderer.js');
+
+        if (Array.isArray(config.resolve.alias)) {
+          config.resolve.alias.push({
+            find: 'electron',
+            replacement: electronjs,
+          });
+        } else {
+          config.resolve.alias['electron'] = electronjs;
+        }
+      },
     },
-  };
-}
+    plugin,
+  ];
+};
 
 /**
- * @type {import('.').GenerateESModule}
+ * @type {(modules: string[]) => Record<string, string>} 
  */
-async function generateESModule(cacheDir, resolve) {
-  // generate custom-resolve module file
-  for (const [module, strOrFn] of Object.entries(resolve)) {
-    const moduleId = path.join(cacheDir, module + '.js');
-    const moduleContent = typeof strOrFn === 'function' ? strOrFn() : strOrFn;
+function builtinModulesExport(modules) {
+  return modules.map((moduleId) => {
+    const nodeModule = require(moduleId)
+    const requireModule = `const M = require("${moduleId}");`
+    const exportDefault = `export default M;`
+    const exportMembers = Object.keys(nodeModule).map(attr => `export const ${attr} = M.${attr}`).join(';\n') + ';'
+    const nodeModuleCode = `
+${requireModule}
+${exportDefault}
+${exportMembers}
+`
 
-    // supported nest module ('@scope/name')
-    ensureDir(path.parse(moduleId).dir);
-    // write custom-resolve
-    fs.writeFileSync(
-      moduleId,
-      moduleContent instanceof Promise ? await moduleContent : moduleContent,
-    );
-  }
-}
-
-/**
- * @type {import('.').ModifyAlias}
- */
-function modifyAlias(config, aliaList) {
-  if (!config.resolve) config.resolve = {};
-
-  let alias = config.resolve.alias || [];
-  if (!Array.isArray(alias)) {
-    // keep the the original alias
-    alias = Object.entries(alias).map(([k, v]) => ({ find: k, replacement: v }));
-  }
-  alias.push(...aliaList.map(item => {
-    const [find, replacement] = Object.entries(item)[0];
-    return { find, replacement };
-  }));
-
-  config.resolve.alias = alias;
-}
-
-/**
- * @type {import('.').ModifyOptimizeDepsExclude}
- */
-function modifyOptimizeDepsExclude(config, exclude) {
-  if (!config.optimizeDeps) config.optimizeDeps = {};
-  if (!config.optimizeDeps.exclude) config.optimizeDeps.exclude = [];
-
-  config.optimizeDeps.exclude.push(...exclude);
-}
-
-/**
- * @type {import('.').ModifyRollupExternal}
- */
-function modifyRollupExternal(config) {
-  if (!config.build) config.build = {};
-  if (!config.build.rollupOptions) config.build.rollupOptions = {};
-  if (!config.build.rollupOptions.external) config.build.rollupOptions.external = [];
-
-  let external = config.build.rollupOptions.external;
-  if (Array.isArray(external)) {
-    external = ['electron', ...builtinModules, ...external];
-  } else if (external instanceof RegExp) {
-    external = ['electron', ...builtinModules, external];
-  } else if (typeof external === 'string') {
-    external = ['electron', ...builtinModules, external];
-  } else if (typeof external === 'function') {
-    const fn = external;
-    external = (source, importer, isResolved) => {
-      if (source === 'electron' || builtinModules.includes(source)) {
-        return true;
-      }
-      return fn.call(fn, source, importer, isResolved);
-    }
-  }
-
-  config.build.rollupOptions.external = external;
-}
-
-/**
- * @type {import('.').ModifyOptionsForElectron}
- */
-function modifyOptionsForElectron(config) {
-  // electron use `loadFile` load Renderer-process file
-  if (!config.base) config.base = './';
-
-  if (!config.build) config.build = {};
-
-  // ensure `require('file-paht')` corre
-  if (!config.build.assetsDir) config.build.assetsDir = '';
-
-  if (!config.build.rollupOptions) config.build.rollupOptions = {};
-  if (!config.build.rollupOptions.output) config.build.rollupOptions.output = {};
-  if (Array.isArray(config.build.rollupOptions.output)) {
-    config.build.rollupOptions.output.forEach(output => {
-      if (!output.format) {
-        // the packaged "electron app" should use "cjs"
-        output.format = 'cjs';
-      }
-    });
-  } else {
-    if (!config.build.rollupOptions.output.format) {
-      // the packaged "electron app" should use "cjs"
-      config.build.rollupOptions.output.format = 'cjs';
-    }
-  }
-}
-
-// --------- utils ---------
-
-function ensureDir(dir) {
-  if (!(fs.existsSync(dir) && fs.statSync(dir).isDirectory())) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-  return dir;
-}
-
-function node_modules(root, count = 0) {
-  if (node_modules.p) {
-    return node_modules.p;
-  }
-  const p = path.join(root, 'node_modules');
-  if (fs.existsSync(p)) {
-    return node_modules.p = p;
-  }
-  if (count >= 19) {
-    throw new Error('Can not found node_modules directory.');
-  }
-  return node_modules(path.join(root, '..'), count + 1);
+    return { [moduleId]: nodeModuleCode }
+  }).reduce((memo, item) => Object.assign(memo, item), {})
 }
