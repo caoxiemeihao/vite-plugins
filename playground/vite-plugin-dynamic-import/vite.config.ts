acd42b050ea38960128abaaf6f6a95c5e6e6d9f9
草鞋没号 <308487730@qq.com>
2022-03-28 08:16:27 +0800

    remove @vitejs/plugin-vue

--- playground/vite-plugin-dynamic-import/vite.config.ts
+++ playground/vite-plugin-dynamic-import/vite.config.ts
@@ -1,13 +1,11 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import dynamicImport from 'vite-plugin-dynamic-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     dynamicImport(),
-    vue(),
   ],
   resolve: {
     alias: {
