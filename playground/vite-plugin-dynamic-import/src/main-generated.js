31e995ba2b8935741f2e1cdd84442fbee1ed018a
草鞋没号 <308487730@qq.com>
2022-03-30 08:54:11 +0800

    feat: support matching nested modules

--- playground/vite-plugin-dynamic-import/src/main-generated.js
+++ playground/vite-plugin-dynamic-import/src/main-generated.js
@@ -10,6 +10,14 @@ async function setView3(id) {
   const { msg } = await __variableDynamicImportRuntime2__(`/root/src/views/${id}`);
   document.querySelector(".view").innerHTML = msg;
 }
+async function setView4(id) {
+  const { msg } = await __variableDynamicImportRuntime3__(`./views${id}`);
+  document.querySelector(".view").innerHTML = msg;
+}
+async function setView5(id) {
+  const { msg } = await __variableDynamicImportRuntime4__(`./views${id}.tsx`);
+  document.querySelector(".view").innerHTML = msg;
+}
 const views1 = {
   "foo-alias1": () => setView1("foo")
 };
@@ -22,6 +30,16 @@ const views3 = {
   "baz-alias3": () => setView3("baz"),
   "home-alias3": () => setView3("home")
 };
+const views4 = {
+  "may-foo": () => setView4("/foo"),
+  "may-bar": () => setView4("/bar"),
+  "may-nesting": () => setView4("/nested/nesting"),
+  "may-nesting-dir": () => setView4("/nested/nesting-dir")
+};
+const views5 = {
+  "may-ext-baz": () => setView4("/baz"),
+  "may-ext-nesting-dir": () => setView4("/nested/nesting-dir")
+};
 Object.entries(views1).forEach(([className, cb]) => {
   document.querySelector(`.${className}`).addEventListener("click", cb);
 });
@@ -31,9 +49,15 @@ Object.entries(views2).forEach(([className, cb]) => {
 Object.entries(views3).forEach(([className, cb]) => {
   document.querySelector(`.${className}`).addEventListener("click", cb);
 });
+Object.entries(views4).forEach(([className, cb]) => {
+  document.querySelector(`.${className}`).addEventListener("click", cb);
+});
+Object.entries(views5).forEach(([className, cb]) => {
+  document.querySelector(`.${className}`).addEventListener("click", cb);
+});
 
-// --------- vite-plugin-dynamic-import ---------
-
+        // --------- vite-plugin-dynamic-import ---------
+        
 function __variableDynamicImportRuntime0__(path) {
   switch (path) {
     case '@/views/foo.js':
@@ -81,6 +105,63 @@ function __variableDynamicImportRuntime2__(path) {
     case '/root/src/views/home/index':
     case '/root/src/views/home':
       return import('./views/home/index.ts');
+
+    default: return new Promise(function(resolve, reject) {
+      (typeof queueMicrotask === 'function' ? queueMicrotask : setTimeout)(
+        reject.bind(null, new Error("Unknown variable dynamic import: " + path))
+      );
+    })
+  }
+}
+
+function __variableDynamicImportRuntime3__(path) {
+  switch (path) {
+    case './views/bar.mjs':
+    case './views/bar':
+      return import('./views/bar.mjs');
+
+    case './views/foo.js':
+    case './views/foo':
+      return import('./views/foo.js');
+
+    case './views/baz/index.tsx':
+    case './views/baz/index':
+    case './views/baz':
+      return import('./views/baz/index.tsx');
+
+    case './views/home/index.ts':
+    case './views/home/index':
+    case './views/home':
+      return import('./views/home/index.ts');
+
+    case './views/nested/nesting.ts':
+    case './views/nested/nesting':
+      return import('./views/nested/nesting.ts');
+
+    case './views/nested/nesting-dir/index.tsx':
+    case './views/nested/nesting-dir/index':
+    case './views/nested/nesting-dir':
+      return import('./views/nested/nesting-dir/index.tsx');
+
+    default: return new Promise(function(resolve, reject) {
+      (typeof queueMicrotask === 'function' ? queueMicrotask : setTimeout)(
+        reject.bind(null, new Error("Unknown variable dynamic import: " + path))
+      );
+    })
+  }
+}
+
+function __variableDynamicImportRuntime4__(path) {
+  switch (path) {
+    case './views/baz/index.tsx':
+    case './views/baz/index':
+    case './views/baz':
+      return import('./views/baz/index.tsx');
+
+    case './views/nested/nesting-dir/index.tsx':
+    case './views/nested/nesting-dir/index':
+    case './views/nested/nesting-dir':
+      return import('./views/nested/nesting-dir/index.tsx');
 
     default: return new Promise(function(resolve, reject) {
       (typeof queueMicrotask === 'function' ? queueMicrotask : setTimeout)(
--- playground/vite-plugin-dynamic-import/src/main.ts
+++ playground/vite-plugin-dynamic-import/src/main.ts
@@ -11,6 +11,16 @@ async function setView3(id: string) {
   const { msg } = await import(`/root/src/views/${id}`)
   document.querySelector('.view')!.innerHTML = msg
 }
+async function setView4(id: string) {
+  // This will match all modules under `views` as far as possible
+  const { msg } = await import(`./views${id}`)
+  document.querySelector('.view')!.innerHTML = msg
+}
+async function setView5(id: string) {
+  // This will match all modules ending in `.tsx` under `views` as far as possible
+  const { msg } = await import(`./views${id}.tsx`)
+  document.querySelector('.view')!.innerHTML = msg
+}
 
 const views1 = {
   'foo-alias1': () => setView1('foo'),
@@ -24,6 +34,16 @@ const views3 = {
   'baz-alias3': () => setView3('baz'),
   'home-alias3': () => setView3('home'),
 }
+const views4 = {
+  'may-foo': () => setView4('/foo'),
+  'may-bar': () => setView4('/bar'),
+  'may-nesting': () => setView4('/nested/nesting'),
+  'may-nesting-dir': () => setView4('/nested/nesting-dir'),
+}
+const views5 = {
+  'may-ext-baz': () => setView4('/baz'),
+  'may-ext-nesting-dir': () => setView4('/nested/nesting-dir'),
+}
 
 Object.entries(views1).forEach(([className, cb]) => {
   document.querySelector(`.${className}`)!.addEventListener('click', cb)
@@ -32,5 +52,11 @@ Object.entries(views2).forEach(([className, cb]) => {
   document.querySelector(`.${className}`)!.addEventListener('click', cb)
 })
 Object.entries(views3).forEach(([className, cb]) => {
+  document.querySelector(`.${className}`)!.addEventListener('click', cb)
+})
+Object.entries(views4).forEach(([className, cb]) => {
+  document.querySelector(`.${className}`)!.addEventListener('click', cb)
+})
+Object.entries(views5).forEach(([className, cb]) => {
   document.querySelector(`.${className}`)!.addEventListener('click', cb)
 })
