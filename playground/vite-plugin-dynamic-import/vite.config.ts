da1c05321dbffc8e63867e9df681a5134aa7168d
草鞋没号 <308487730@qq.com>
2022-04-01 15:41:42 +0800

    use relative path

--- playground/vite-plugin-dynamic-import/vite.config.ts
+++ playground/vite-plugin-dynamic-import/vite.config.ts
@@ -1,6 +1,6 @@
 import { join } from 'path'
 import { defineConfig } from 'vite'
-import dynamicImport from 'vite-plugin-dynamic-import'
+import dynamicImport from '../../packages/dynamic-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
