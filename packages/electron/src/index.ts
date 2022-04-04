2d4b5dc4e2beae1bcc30566e0b6c86d5c7349869
草鞋没号 <308487730@qq.com>
2022-04-05 07:13:47 +0800

    migrate packages/electron to vite-plugin-electron

--- packages/electron/src/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Configuration } from './types'
-
-export { electron as default } from './plugin'
-export { Configuration }
-
-export function defineConfig(config: Configuration) {
-  return config
-}
