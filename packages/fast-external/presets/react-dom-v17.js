9fa7a901d731779f28981b4a53375dedcbb93eb6
草鞋没号 <308487730@qq.com>
2022-04-16 09:45:13 +0800

    react-dom-v17.js

---
+++ packages/fast-external/presets/react-dom-v17.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'ReactDOM',
+  members: [
+    '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+    'createPortal',
+    'findDOMNode',
+    'flushSync',
+    'hydrate',
+    'render',
+    'unmountComponentAtNode',
+    'unstable_batchedUpdates',
+    'unstable_createPortal',
+    'unstable_renderSubtreeIntoContainer',
+    'version',
+  ],
+};
