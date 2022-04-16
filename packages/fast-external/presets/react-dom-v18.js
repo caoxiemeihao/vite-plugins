e113651f8c17bdb8e0dc39fc121b92fa91288603
草鞋没号 <308487730@qq.com>
2022-04-16 09:45:22 +0800

    react-dom-v18.js

---
+++ packages/fast-external/presets/react-dom-v18.js
@@ -0,0 +1,20 @@
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'ReactDOM',
+  members: [
+    '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+    'createPortal',
+    'createRoot',
+    'findDOMNode',
+    'flushSync',
+    'hydrate',
+    'hydrateRoot',
+    'render',
+    'unmountComponentAtNode',
+    'unstable_batchedUpdates',
+    'unstable_renderSubtreeIntoContainer',
+    'version',
+  ],
+};
