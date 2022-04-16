f00672511c39e979bd0ee07ce63f004b6b09eb4f
草鞋没号 <308487730@qq.com>
2022-04-16 09:45:45 +0800

    vue-v2.js

---
+++ packages/fast-external/presets/vue-v2.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'Vue',
+  members: [
+    'util',
+    'set',
+    'delete',
+    'nextTick',
+    'observable',
+    'options',
+    'use',
+    'mixin',
+    'cid',
+    'extend',
+    'component',
+    'directive',
+    'filter',
+    'version',
+    'compile',
+  ],
+};
