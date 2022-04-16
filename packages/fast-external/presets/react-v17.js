086577061fa00735c4349319bbdb9e1723229c9a
草鞋没号 <308487730@qq.com>
2022-04-16 09:45:30 +0800

    react-v17.js

---
+++ packages/fast-external/presets/react-v17.js
@@ -0,0 +1,36 @@
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'React',
+  members: [
+    'Fragment',
+    'StrictMode',
+    'Profiler',
+    'Suspense',
+    'Children',
+    'Component',
+    'PureComponent',
+    '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
+    'cloneElement',
+    'createContext',
+    'createElement',
+    'createFactory',
+    'createRef',
+    'forwardRef',
+    'isValidElement',
+    'lazy',
+    'memo',
+    'useCallback',
+    'useContext',
+    'useDebugValue',
+    'useEffect',
+    'useImperativeHandle',
+    'useLayoutEffect',
+    'useMemo',
+    'useReducer',
+    'useRef',
+    'useState',
+    'version',
+  ],
+};
