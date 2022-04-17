51f6eca2c1d4dd8c18d0b75f204ee2009fb20447
草鞋没号 <308487730@qq.com>
2022-04-17 08:43:59 +0800

    refactor: only one plugin

--- packages/fast-external/index.js
+++ packages/fast-external/index.js
@@ -1,44 +1,38 @@
 /**
- * @type {import(".").VitePluginFastExternal}
+ * @type {import('.').VitePluginFastExternal}
  */
 module.exports = function external(entries) {
-  const name = 'vite-plugin-fast-external';
   const externalId = '__fast-external:';
   const keys = Object.keys(entries);
 
-  return [
-    {
-      name: `${name}:resolveId`,
-      enforce: 'pre',
-      resolveId(source) {
-        if (keys.includes(source)) {
-          // avoid vite builtin `vite:resolve` plugin
-          return externalId + source;
-        }
-      },
+  return {
+    name: 'vite-plugin-fast-external',
+    enforce: 'pre',
+    resolveId(source) {
+      if (keys.includes(source)) {
+        // avoid vite builtin `vite:resolve` plugin
+        return externalId + source;
+      }
     },
-    {
-      name,
-      config(config) {
-        if (!config.optimizeDeps) config.optimizeDeps = {};
-        if (!config.optimizeDeps.exclude) config.optimizeDeps.exclude = [];
+    config(config) {
+      if (!config.optimizeDeps) config.optimizeDeps = {};
+      if (!config.optimizeDeps.exclude) config.optimizeDeps.exclude = [];
 
-        let exclude = keys;
-        if (config.optimizeDeps.include) {
-          exclude = keys.filter(key => !config.optimizeDeps.include.includes(key));
-        }
-        config.optimizeDeps.exclude.push(...exclude);
-      },
-      load(id) {
-        if (id.startsWith(externalId)) {
-          const module = id.split(externalId)[1];
-          const fnOrIife = entries[module];
-
-          return typeof fnOrIife === 'function'
-            ? fnOrIife(id)
-            : `const M = window['${fnOrIife}']; export { M as default }`;
-        }
-      },
+      let exclude = keys;
+      if (config.optimizeDeps.include) {
+        exclude = keys.filter(key => !config.optimizeDeps.include.includes(key));
+      }
+      config.optimizeDeps.exclude.push(...exclude);
     },
-  ];
+    load(id) {
+      if (id.startsWith(externalId)) {
+        const module = id.split(externalId)[1];
+        const fnOrIife = entries[module];
+
+        return typeof fnOrIife === 'function'
+          ? fnOrIife(id)
+          : `const M = window['${fnOrIife}']; export { M as default }`;
+      }
+    },
+  };
 };
