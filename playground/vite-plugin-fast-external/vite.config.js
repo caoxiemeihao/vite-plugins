6debb67137d6230ad6b186ee2e720dcc681242eb
草鞋没号 <308487730@qq.com>
2022-04-22 08:51:29 +0800

    chore: update use case

--- playground/vite-plugin-fast-external/vite.config.js
+++ playground/vite-plugin-fast-external/vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite';
-import external from 'vite-plugin-fast-external';
+import external from '../../packages/fast-external';
+import { vue_v3, pinia_v2, lib2external } from '../../packages/fast-external/presets';
 
 export default defineConfig({
   plugins: [
     external({
-      vue: 'Vue',
       '@angular/core': () => `const Angular = { version: '13.3.0' }; export default Angular;`,
+      vue: vue_v3,
+      pinia: pinia_v2,
+      'vue-router': lib2external('VueRouter', [
+        'NavigationFailureType',
+        'RouterLink',
+        'RouterView',
+        'START_LOCATION',
+        'createMemoryHistory',
+        'createRouter',
+        'createRouterMatcher',
+        'createWebHashHistory',
+        'createWebHistory',
+        'isNavigationFailure',
+        'matchedRouteKey',
+        'onBeforeRouteLeave',
+        'onBeforeRouteUpdate',
+        'parseQuery',
+        'routeLocationKey',
+        'routerKey',
+        'routerViewLocationKey',
+        'stringifyQuery',
+        'useLink',
+        'useRoute',
+        'useRouter',
+        'viewDepthKey',
+      ]),
     }),
   ],
 });
