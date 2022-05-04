41dcbab0de0050b2dbd6479bdd0b736b6d345bfd
草鞋没号 <308487730@qq.com>
2022-05-05 07:57:10 +0800

    react-router-v6.js

---
+++ packages/fast-external/presets/react-router-v6.js
@@ -0,0 +1,38 @@
+
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'ReactRouter',
+  members: [
+    'NavigationType',
+    'createPath',
+    'parsePath',
+    'MemoryRouter',
+    'Navigate',
+    'Outlet',
+    'Route',
+    'Router',
+    'Routes',
+    'UNSAFE_LocationContext',
+    'UNSAFE_NavigationContext',
+    'UNSAFE_RouteContext',
+    'createRoutesFromChildren',
+    'generatePath',
+    'matchPath',
+    'matchRoutes',
+    'renderMatches',
+    'resolvePath',
+    'useHref',
+    'useInRouterContext',
+    'useLocation',
+    'useMatch',
+    'useNavigate',
+    'useNavigationType',
+    'useOutlet',
+    'useOutletContext',
+    'useParams',
+    'useResolvedPath',
+    'useRoutes',
+  ],
+};
