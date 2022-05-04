b38cfc28a83560516bcc0ffca7b475f7787dfa3c
草鞋没号 <308487730@qq.com>
2022-05-05 07:57:02 +0800

    react-router-v5.js

---
+++ packages/fast-external/presets/react-router-v5.js
@@ -0,0 +1,25 @@
+
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'ReactRouter',
+  members: [
+    'MemoryRouter',
+    'Prompt',
+    'Redirect',
+    'Route',
+    'Router',
+    'StaticRouter',
+    'Switch',
+    '__HistoryContext',
+    '__RouterContext',
+    'generatePath',
+    'matchPath',
+    'useHistory',
+    'useLocation',
+    'useParams',
+    'useRouteMatch',
+    'withRouter',
+  ],
+};
