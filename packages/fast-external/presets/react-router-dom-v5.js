6a9fd09cebbabb1fa40f3415a59801521dc05f21
草鞋没号 <308487730@qq.com>
2022-05-05 07:56:30 +0800

    react-router-dom-v5.js

---
+++ packages/fast-external/presets/react-router-dom-v5.js
@@ -0,0 +1,27 @@
+
+/**
+ * @type {import('.').LibMeta}
+ */
+module.exports = {
+  name: 'ReactRouterDOM',
+  members: [
+    'BrowserRouter',
+    'HashRouter',
+    'Link',
+    'MemoryRouter',
+    'NavLink',
+    'Prompt',
+    'Redirect',
+    'Route',
+    'Router',
+    'StaticRouter',
+    'Switch',
+    'generatePath',
+    'matchPath',
+    'useHistory',
+    'useLocation',
+    'useParams',
+    'useRouteMatch',
+    'withRouter',
+  ],
+};
