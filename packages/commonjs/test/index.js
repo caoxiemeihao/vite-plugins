546589e8b1281b16e38487ff74f2bbf3db974d57
草鞋没号 <308487730@qq.com>
2022-07-23 09:34:32 +0800

    🚨: remove migrated repository

--- packages/commonjs/test/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const Module = require('../dist/cjs-esm/Module').default;
-
-const requireJs = path.join(__dirname, 'require.js');
-const code = fs.readFileSync(requireJs, 'utf8');
-
-new Module(code, requireJs).analyze();
--- playground/vite-plugin-dynamic-import/src/views/baz/index.tsx
+++
@@ -1,1 +0,0 @@
-export const msg = 'baz/index.tsx'
